23fb02634860ba40fbc1b69a94d36bc34341ed7b
masaya-tn <ydongnei@gmail.com>
2023-06-28 23:50:58 +0900

    ログイン機能実装

--- main.go
+++ main.go
@@ -11,4 +11,15 @@ func main() {
 	fmt.Println(models.Db)
 
 	controllers.StartMainServer()
+	// user, _ := models.GetUserByEmail("f@f.com")
+	// fmt.Println(user)
+
+	// session, err := user.CreateSession()
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// fmt.Println(session)
+
+	// valid, _ := session.CheckSession()
+	// fmt.Println(valid)
 }
